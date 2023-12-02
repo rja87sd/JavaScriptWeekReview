e75a8c46aeb3a23db90896f93c34812a76a646b6
Ryan Arnold <rja87.sd@gmail.com>
2023-12-01 20:17:56 -0600

    Create form for Name and Age and button with JS functionality.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const message = document.querySelector('#message');
+const colors = ["Red", "Blue", "Green", "Yellow", "Purple"];
+
+const ul = document.querySelector('ul');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const li = document.createElement('li');
+    const name = e.target.name.value;
+    const age = e.target.age.value;
+    li.innerText = `Name: ${name} Age: ${age}`;
+    ul.appendChild(li);
+});
